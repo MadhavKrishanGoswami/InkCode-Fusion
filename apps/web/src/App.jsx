b37310b44f93726046dc659a167d6074ee876279
Madhav Krishan Goswami <116915826+MadhavKrishanGoswami@users.noreply.github.com>
2024-02-02 23:26:39 +0530

    fix for mobile

--- apps/web/src/App.jsx
+++ apps/web/src/App.jsx
@@ -2,9 +2,22 @@ import "./App.css";
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import AnimatedRoutes from "./components/Transition/AnimatedRoutes";
+import Cursor from "../src/pages/GradientBubbles/GradientBubbles";
 
 import { Toaster } from "react-hot-toast";
 function App() {
+  const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+  if (isMobile) {
+    return (
+      <div className=" ">
+        <h1 className=" absolute z-50 text-[7vw] mt-[40vh] content-center justify-center text-Tblack tracking-tighter font-normal max-w-screen text-center w-[50%]  ml-[25vw]">
+          Sorry, InkCode is not supported on mobile devices Yet :(
+        </h1>
+        <Cursor />
+      </div>
+    );
+  }
+
   return (
     <>
       <div>
