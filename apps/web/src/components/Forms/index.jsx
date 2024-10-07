a1fa998e75dd6323d25a86f269f7979d018f747c
Sohel Sheikh <sohelxsheikh@gmail.com>
2024-10-07 22:46:58 +0530

    Changed the color

--- apps/web/src/components/Forms/index.jsx
+++ apps/web/src/components/Forms/index.jsx
@@ -14,7 +14,7 @@ const Forms = () => {
         </h1>
         <CreateRoomForm />
       </div>
-      <div className="flex flex-col items-center bg-StartBg w-[30vw] h-[35vw] rounded-3xl shadow-md shadow-Tblack">
+      <div className="flex flex-col items-center bg-black w-[30vw] h-[35vw] rounded-3xl shadow-md shadow-Tblack">
         <h1
           className="flex justify-center items-center relative font-bold top-[3.4vw]
         leading-normal capitalize flex-shrink-0 text-center text-[2vw] font-[Poppins] text-StartHead"
